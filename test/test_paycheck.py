e7821c3de09632207c91ed6993b252846ec4ef29
Kaio Cesar <tecnico.kaio@gmail.com>
2019-11-19 21:55:08 -0300

    test passed

--- test/test_paycheck.py
+++ test/test_paycheck.py
@@ -1,6 +1,40 @@
 import unittest
 
+# Calculo do INSS: 
+# Calculo do FGTS: 
+# Calculo de dependentes: 
+# Calculo do IRRF
+# Calcular DSR (Descanso Semanal Remunerado)
+# Calculo de vale transporte
+# Calculo de adicional noturno
+# Calcular adicional insalubridade
+# Calcular Salario Liquido
+
 class test_paycheck(unittest.TestCase):
 
-    def test_dummy_test(self):
+    def test_calcular_inss(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_fgts(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_dependentes(self):
+        self.assertEqual(1,1)
+    
+    def test_calcular_irrf(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_dsr(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_vale_transporte(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_adicional_noturno(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_adicional_insalubridade(self):
+        self.assertEqual(1,1)
+
+    def test_calcular_salario_liquido(self):
         self.assertEqual(1,1)
